1f779b4ee581d6e37c74203e0bca78b8d9ee09ae
17Divyaantil <divyaantil33@gmail.com>
2022-11-02 23:15:45 +0530

    code fixed for prime

--- prime.js
+++ prime.js
@@ -1,9 +1,18 @@
-function checkPrime(num){
- for (let i=0;i<=num;i++){
- if (num%i){
+function checkPrime(number){
+let factors=0;
+ for (let i=0;i<=number;i++){
+ if (number%i==0){
+factors++;    }
+}
+if (factors ==2){
  return true;
  }
- }
+
  return false;
  }
- checkPrime(13);
+ let answer= checkPrime(13);
+if( answer==true){
+consoole.log("Prime Number");
+} else {
+console.log("Not a Prime Number");
+}
